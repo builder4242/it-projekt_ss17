ca8fbd2e89c4f831e717d6a302c3aff862950ce4
Daniel Miedtank <d.miedtank@miedtank.com>
2017-05-09 16:54:25 +0200

    Partnerprofil zur Ausschreibung ergänzt

--- src/de/hdm/it_projekt/shared/bo/Ausschreibung.java
+++ src/de/hdm/it_projekt/shared/bo/Ausschreibung.java
@@ -4,73 +4,98 @@ import java.util.Date;
 
 /**
  * Klasse Ausschreibung (Unterklasse von BusinessObject)
+ * 
  * @author Sid Heiland
  *
  */
 public class Ausschreibung extends BusinessObject {
 
-/**
- * Bezeichnung der Ausschreibung
- */
+	/**
+	 * Bezeichnung der Ausschreibung
+	 */
 	private String bezeichnung;
 
-/**
- * Bewerbungsfrist der Ausschreibung
- */
+	/**
+	 * Bewerbungsfrist der Ausschreibung
+	 */
 	private Date bewerbungsfrist;
 
-/**
- * Text der Ausschreibung
- */
+	/**
+	 * Text der Ausschreibung
+	 */
 	private String ausschreibungstext;
 
-/**
- * Auslesen der Bezeichnung der Ausschreibung
- * @return bezeichnung
- */
+	/**
+	 * Partnerprofil zur Ausschreibung
+	 */
+	private Partnerprofil partnerprofil = null;
+
+	/**
+	 * Auslesen der Bezeichnung der Ausschreibung
+	 * 
+	 * @return bezeichnung
+	 */
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
-	
-/**
- * Setzen der Bezeichnung der Ausschreibung
- * @param bezeichnung
- */
+
+	/**
+	 * Setzen der Bezeichnung der Ausschreibung
+	 * 
+	 * @param bezeichnung
+	 */
 	public void setBezeichnung(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
-	
-/**
- * Auslesen der Bewerbungsfrist der Ausschreibung
- * @return bewerbungsfrist
- */
+
+	/**
+	 * Auslesen der Bewerbungsfrist der Ausschreibung
+	 * 
+	 * @return bewerbungsfrist
+	 */
 	public Date getBewerbungsfrist() {
 		return bewerbungsfrist;
 	}
-	
-/**
- * Setzen der Bewerbungsfrist der Ausschreibung
- * @param bewerbungsfrist
- */
+
+	/**
+	 * Setzen der Bewerbungsfrist der Ausschreibung
+	 * 
+	 * @param bewerbungsfrist
+	 */
 	public void setBewerbungsfrist(Date bewerbungsfrist) {
 		this.bewerbungsfrist = bewerbungsfrist;
 	}
-	
-/**
- * Auslesen des Ausschreibungstext der Ausschreibung
- * @return ausschreibungstext
- */
+
+	/**
+	 * Auslesen des Ausschreibungstext der Ausschreibung
+	 * 
+	 * @return ausschreibungstext
+	 */
 	public String getAusschreibungstext() {
 		return ausschreibungstext;
 	}
-	
-/**
- * Setzen des Ausschreibungstext der Ausschreibung
- * @param ausschreibungstext
- */
+
+	/**
+	 * Setzen des Ausschreibungstext der Ausschreibung
+	 * 
+	 * @param ausschreibungstext
+	 */
 	public void setAusschreibungstext(String ausschreibungstext) {
 		this.ausschreibungstext = ausschreibungstext;
 	}
-	
-	
+		
+	/**
+	 * @return the partnerprofil
+	 */
+	public Partnerprofil getPartnerprofil() {
+		return partnerprofil;
+	}
+
+	/**
+	 * @param partnerprofil the partnerprofil to set
+	 */
+	public void setPartnerprofil(Partnerprofil partnerprofil) {
+		this.partnerprofil = partnerprofil;
+	}
+
 }
