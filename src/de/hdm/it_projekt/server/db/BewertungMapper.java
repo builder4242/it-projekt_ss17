1df06fca91ac3a7bf67a28b39c3dcd03083e2009
YavuzElif <ey004@hdm-stuttgart.de>
2017-05-21 14:53:56 +0200

    Statische Methode und geschuetzten Konstruktor erstellt.

--- src/de/hdm/it_projekt/server/db/BewertungMapper.java
+++ src/de/hdm/it_projekt/server/db/BewertungMapper.java
@@ -1,12 +1,55 @@
-/**
- * 
- */
+
 package de.hdm.it_projekt.server.db;
 
 /**
+ * Anlehnung an @author Thies
  * @author ElifY
- *
+ * 
+ * Mapper-Klasse, die <code>Bewertung</code>-Objekte auf eine relationale
+ * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfügung
+ * gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
+ * gelöscht werden können. Das Mapping ist bidirektional. D.h., Objekte können
+ * in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
  */
 public class BewertungMapper {
 
+	/**
+	   * Die Klasse BewertungMapper wird nur einmal instantiiert. Man spricht
+	   * hierbei von einem sogenannten <b>Singleton</b>.
+	   * <p>
+	   * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal für
+	   * sämtliche eventuellen Instanzen dieser Klasse vorhanden. Sie speichert die
+	   * einzige Instanz dieser Klasse.
+	   * 
+	   */
+	  private static BewertungMapper bewertungMapper = null;
+
+	  /**
+	   * Geschützter Konstruktor - verhindert die Möglichkeit, mit <code>new</code>
+	   * neue Instanzen dieser Klasse zu erzeugen. 
+	   */
+	  protected BewertungMapper() {
+	  }
+
+	  /**
+	   * Diese statische Methode kann aufgrufen werden durch
+	   * <code>BewertungMapper.bewertungMapper()</code>. Sie stellt die
+	   * Singleton-Eigenschaft sicher, indem Sie dafür sorgt, dass nur eine einzige
+	   * Instanz von <code>BewertungMapper</code> existiert.
+	   * <p>
+	   * 
+	   * <b>Fazit:</b> BewertungMapper sollte nicht mittels <code>new</code>
+	   * instantiiert werden, sondern stets durch Aufruf dieser statischen Methode.
+	   * 
+	   * @return DAS <code>BewertungMapper</code>-Objekt.
+	   */
+	  public static BewertungMapper bewertungMapper() {
+	    if (bewertungMapper == null) {
+	      bewertungMapper = new BewertungMapper();
+	    }
+
+	    return bewertungMapper;
+	  }
+
 }
+
