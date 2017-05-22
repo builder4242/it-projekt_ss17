74a6cabca35d1c3d4bc3aa8a674f11cb9aaecd7a
TugbaBulat <tb103@hdm-stuttgart.de>
2017-05-22 20:31:54 +0200

    Einfuegen der Singleton-Eigenschaft, des geschuetzten Konstruktors und der statischen Methode in die Klasse PartnerprofilMapper.

--- src/de/hdm/it_projekt/server/db/PartnerprofilMapper.java
+++ src/de/hdm/it_projekt/server/db/PartnerprofilMapper.java
@@ -4,9 +4,63 @@
 package de.hdm.it_projekt.server.db;
 
 /**
- * @author Tugba
+ * Mapper-Klasse, die <code>Partnerprofil</code>-Objekte auf eine relationale
+ * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfuegung
+ * gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
+ * geloescht werden koennen. Das Mapping ist bidirektional. D.h., Objekte koennen
+ * in DB-Strukturen und DB-Strukturen in Objekte umgewandelt werden.
+ * <p>
+ * 
+ * Anlehnung an @author Thies
+ * @author Tugba Bulat
  *
  */
 public class PartnerprofilMapper {
+
+	
+	
+	/**
+	 * Die Klasse PartnerprofilMapper wird nur einmal instantiiert.
+	 * Man spricht hierbei von einem sogenannten <b>Singleton</b>.
+	 * <p>
+	 * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal für
+	 * saemtliche eventuellen Instanzen dieser Klasse vorhanden.
+	 * Sie speichert die einzige Instanz dieser Klasse.
+	 * 
+	 */
+	private static PartnerprofilMapper partnerprofilMapper = null;
+	
+	
+	/***
+	 * Geschuetzer Konstruktor - verhindert die Moeglichkeit, mit new neue
+	 * Instanzen dieser Klasse zu erzeugen.
+	 */
+	protected PartnerprofilMapper(){
+		
+	}
+	
+	
+	
+	/**
+	 * Diese statische Methode kann aufgrufen werden durch
+	 * <code>PartnerprofilMapper.partnerprofilMapper()</code>.
+	 * Sie stellt die Singleton-Eigenschaft sicher,
+	 * indem Sie dafuer sorgt, dass nur eine einzige
+	 * Instanz von <code>PartnerprofilMapper</code> existiert.
+	 * <p>
+	 * 
+	 * <b>Fazit:</b> PartnerprofilMapper sollte nicht mittels <code>new</code>
+	 * instantiiert werden, sondern stets durch Aufruf dieser statischen Methode.
+
+	 * @return DAS <code>PartnerprofilMapper</code>-Objekt
+
+	 */
+	public static PartnerprofilMapper partnerprofilMapper() {
+	    if (partnerprofilMapper == null) {
+	    	partnerprofilMapper = new PartnerprofilMapper();
+	    }
+
+	    return partnerprofilMapper;
+	  }
 
 }
