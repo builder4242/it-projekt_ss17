34e8b4cf80e3c38c818bd4756c717b48b8764eaf
TugbaBulat <tb103@hdm-stuttgart.de>
2017-05-22 19:35:28 +0200

    Einfuegen der Singleton-Eigenschaft, des geschützten Konstruktors und der statischen Methode in der Klasse UnternehmenMapper.

--- src/de/hdm/it_projekt/server/db/UnternehmenMapper.java
+++ src/de/hdm/it_projekt/server/db/UnternehmenMapper.java
@@ -16,5 +16,49 @@ package de.hdm.it_projekt.server.db;
  *
  */
 public class UnternehmenMapper {
+	
+	/**
+	 * Die Klasse UnternehmenMapper wird nur einmal instantiiert.
+	 * Man spricht hierbei von einem sogenannten <b>Singleton</b>.
+	 * <p>
+	 * Diese Variable ist durch den Bezeichner <code>static</code>
+	 * nur einmal für saemtliche eventuellen Instanzen dieser Klasse vorhanden.
+	 * Sie speichert die einzige Instanz dieser Klasse.
+	 * 
+	 */
+	private static UnternehmenMapper unternehmenMapper = null;
+	
+	
+	
+	/***
+	 * Geschuetzter Konstruktor - verhindert die Moeglichkeit, mit new neue
+	 * Instanzen dieser Klasse zu erzeugen
+	 */
+	protected UnternehmenMapper(){
+		
+	}
 
+	
+	/**
+	   * Diese statische Methode kann aufgrufen werden durch
+	   * <code>UnternehmenMapper.unternehmenMapper()</code>.
+	   * Sie stellt die Singleton-Eigenschaft sicher,
+	   * indem Sie dafuer sorgt, dass nur eine einzige
+	   * Instanz von <code>UnternehmenMapper</code> existiert.
+	   * <p>
+	   * 
+	   * <b>Fazit:</b> UnternehmenMapper sollte nicht mittels <code>new</code>
+	   * instantiiert werden, sondern stets durch Aufruf dieser statischen Methode.
+	   * 
+	   * @return DAS <code>UnternehmenMapper</code>-Objekt.
+	   */
+	public static UnternehmenMapper unternehmenMapper() {
+	    if (unternehmenMapper == null) {
+	    	unternehmenMapper = new UnternehmenMapper();
+	    }
+
+	    return unternehmenMapper;
+	  }
+
+	
 }
