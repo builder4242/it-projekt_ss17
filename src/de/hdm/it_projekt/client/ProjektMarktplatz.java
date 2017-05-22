16c4ae73feda196ddaf79d685a021861332fa644
TugbaBulat <tb103@hdm-stuttgart.de>
2017-05-22 14:33:31 +0200

    Einfuegen der setID-Methode in die Klasse ProjektMarktplatz.

--- src/de/hdm/it_projekt/client/ProjektMarktplatz.java
+++ src/de/hdm/it_projekt/client/ProjektMarktplatz.java
@@ -1,15 +1,30 @@
 package de.hdm.it_projekt.client;
 
+	/***
+	 * 
+	 * @author Tugba Bulat
+	 *
+	 */
 public class ProjektMarktplatz {
 
+	
 	/***
 	 * Auslesen der ID
 	 * @return
-	 * @author Tugba Bulat
 	 */
 	public static String getID() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	
+	
+	/***
+	 * Setzen der ID
+	 * @param ID
+	 */
+	public void setID(int ID) {
+		// TODO Auto-generated method stub
 	}
 
 }
