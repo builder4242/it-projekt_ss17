477f3bb16bf3ab19bd065d0058dbea42ed5d13b6
TugbaBulat <tb103@hdm-stuttgart.de>
2017-05-22 14:29:54 +0200

    Einfuegen der getID-Methode in die Klasse ProjektMarktplatz.

--- src/de/hdm/it_projekt/client/ProjektMarktplatz.java
+++ src/de/hdm/it_projekt/client/ProjektMarktplatz.java
@@ -2,4 +2,14 @@ package de.hdm.it_projekt.client;
 
 public class ProjektMarktplatz {
 
+	/***
+	 * Auslesen der ID
+	 * @return
+	 * @author Tugba Bulat
+	 */
+	public static String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
