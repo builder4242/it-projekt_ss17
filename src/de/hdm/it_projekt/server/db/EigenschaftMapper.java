ba14219c8b087a42b34bce5bdf0bbafeaef6c801
YavuzElif <ey004@hdm-stuttgart.de>
2017-05-23 11:55:23 +0200

    Insert-Methode erstellt

--- src/de/hdm/it_projekt/server/db/EigenschaftMapper.java
+++ src/de/hdm/it_projekt/server/db/EigenschaftMapper.java
@@ -1,5 +1,12 @@
 package de.hdm.it_projekt.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import de.hdm.it_projekt.shared.bo.Eigenschaft;
+
 /**
  * Mapper-Klasse, die <code>Eigenschaft</code>-Objekte auf eine relationale
  * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfuegung
@@ -51,4 +58,55 @@ public class EigenschaftMapper {
 
     return eigenschaftMapper;
   }
+
+  /**
+   * Einfuegen eines <code>Eigenschaft</code>-Objekts in die Datenbank. Dabei
+   * wird auch der Primaerschluessel des uebergebenen Objekts geprueft und ggf.
+   * berichtigt.
+   * 
+   * @param e das zu speichernde Objekt
+   * @return das bereits uebergeben Objekt, jedoch mit ggf. korrigierter
+   * 		<code>id</id>
+   */
+  		public Eigenschaft insert(Eigenschaft e){
+  			
+  			//DB-Verbindung herstellen
+  			Connection con = DBConnection.connection();
+			
+  			try{
+  				//Leeres SQL-Statement (JDBC) anlegen
+  				Statement stmt = con.createStatement();
+  				
+  				//Momentan hoechsten Primaerschluesselwert pruefen
+  				ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid "
+  			          + "FROM transactions ");
+  				
+  				// Wenn wir etwas zurueckerhalten, kann dies nur einzeilig sein
+  				if (rs.next()) {
+  		    	  
+  		    	  /*
+  		    	   * e erhaelt den bisher maximalen, nun um 1 inkrementierten
+  		    	   * Primaerschluessel.
+  		    	   */
+  		    	  
+  		    	  	e.setId(rs.getInt("maxid") + 1);
+  		    	  	
+  		    	  	stmt = con.createStatement();
+  		    	  	
+  		    	  	// Jetzt erst erfolgt die tatsaechliche Einfuegeoperation
+  			        stmt.executeUpdate("INSERT INTO eigenschaften (id, name, wert) "
+  			            + "VALUES (" + Eigenschaft.getId() + ","
+  			            + e.getName() + ","
+  			            + e.getWert() + ")");
+  			      }
+  			}
+  					catch (SQLException e1) {
+  						e1.printStackTrace();
+  					}
+  					//Rueckgabe, der evtl. korrigierten Beteiligung.
+  					return e;
+  		  }
+  		
 }
+
+
