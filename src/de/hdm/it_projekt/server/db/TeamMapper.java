904ea747da5d8ffe18c8d1b5dda38da79fc94222
TugbaBulat <tb103@hdm-stuttgart.de>
2017-05-28 01:13:53 +0200

    Einfuegen der insert-, update- und delete-Methoden in der Klasse TeamMapper.

--- src/de/hdm/it_projekt/server/db/TeamMapper.java
+++ src/de/hdm/it_projekt/server/db/TeamMapper.java
@@ -3,6 +3,14 @@
  */
 package de.hdm.it_projekt.server.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import de.hdm.it_projekt.shared.bo.Organisationseinheit;
+import de.hdm.it_projekt.shared.bo.Team;
+
 /**
  * Mapper-Klasse, die <code>Team</code>-Objekte auf eine relationale
  * Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfuegung
@@ -58,6 +66,84 @@ public class TeamMapper {
 		}
 
 		return teamMapper;
+	}
+
+	
+	/**
+	 * Die Methode ermoeglicht es Team-Objekte in der Datenbank anzulegen. 
+	 * 
+	 * @param t - das in der Datenbank anzulegende Objekt
+	 * @return
+	 * @throws Exception
+	 */
+	public Team insert(Team t) throws Exception {
+
+		// DB-Verbindung herstellen
+		Connection con = DBConnection.connection();
+		PreparedStatement preStmt = null;
+
+		try {
+			String sql = "INSERT INTO `Team`(`ID`) VALUES (NULL)";
+
+			preStmt = con.prepareStatement(sql);
+			preStmt.executeUpdate();
+			preStmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Datenbank fehler!" + e.toString());
+		}
+		return t;
+	}
+
+	
+	/**
+	 * Wiederholtes Schreiben eines Team-Objekts in die Datenbank.
+	 * 
+	 * @param t - das Objekt, das in die Datenbank geschrieben werden soll
+	 * @return
+	 */
+	public Team update(Team t) {
+
+		// DB-Verbindung herstellen
+		Connection con = DBConnection.connection();
+
+		try {
+			Statement stmt = con.createStatement();
+
+			stmt.executeUpdate("UPDATE Team " + "SET name=\"" + Organisationseinheit.getName() + "\" " + "SET email=\""
+					+ Organisationseinheit.getEmail() + "\" " + "SET strasse=\"" + Organisationseinheit.getStrasse()
+					+ "\" " + "SET plz=\"" + Organisationseinheit.getPLZ() + "\" " + "SET ort=\""
+					+ Organisationseinheit.getOrt() + "\" " + "SET tel=\"" + Organisationseinheit.getTel() + "\" "
+					+ "SET googleID=\"" + Organisationseinheit.getGoogleID() + "\" " + "WHERE ID="
+					+ Organisationseinheit.getID());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// Um Analogie zu insert(Team t) zu wahren,
+		// geben wir t zurueck.
+		return t;
+	}
+	
+	
+	/**
+	 * Loeschen der Daten eines Team-Objekts aus der Datenbank.
+	 * 
+	 * @param t - das zu loeschende Objekt
+	 */
+	public void delete (Team t){
+
+		// DB-Verbindung herstellen
+		Connection con = DBConnection.connection();
+		Statement stmt = null;
+
+		try {
+			stmt = con.createStatement();
+			stmt.executeUpdate("DELETE FROM Team " + "WHERE ID=" + Organisationseinheit.getID());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
