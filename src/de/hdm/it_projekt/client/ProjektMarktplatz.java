e2c7df1bfd3e4c9cd107c2647f2eda82cfb032e0
Daniel Miedtank <d.miedtank@miedtank.com>
2017-05-30 12:50:03 +0200

    Client EntryPoint angelegt

--- src/de/hdm/it_projekt/client/ProjektMarktplatz.java
+++ src/de/hdm/it_projekt/client/ProjektMarktplatz.java
@@ -1,51 +1,12 @@
 package de.hdm.it_projekt.client;
 
-	/***
-	 * 
-	 * @author Tugba Bulat
-	 *
-	 */
-public class ProjektMarktplatz {
+import com.google.gwt.core.client.*;
 
-	/***
-	 * Setzen der ID
-	 * @param ID
-	 */
-	public void setID(int ID) {
-		// TODO Auto-generated method stub
-	}
+public class ProjektMarktplatz implements EntryPoint {
 
-
-	
-	/***
-	 * Auslesen der ID
-	 * @return
-	 */
-	public static String getID() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	
-	/***
-	 * Setzen der Bezeichnung
-	 * @param bz
-	 */
-	public void setBezeichnung(String bezeichnung) {
+	public void onModuleLoad() {
 		// TODO Auto-generated method stub
 		
-	}
-
-
-
-	/***
-	 * Auslesen der Bezeichnung
-	 * @return
-	 */
-	public static String getBezeichnung() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
