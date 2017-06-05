2c6a3848f4ae197a012e505c67f1d835fd93b54a
Daniel <Daniel@DESKTOP-EO7Q18C>
2017-06-05 17:35:22 +0200

    div hinzugefügt

--- src/de/hdm/it_projekt/client/ProjektMarktplatz.java
+++ src/de/hdm/it_projekt/client/ProjektMarktplatz.java
@@ -30,7 +30,7 @@ public class ProjektMarktplatz implements EntryPoint {
 		
 		
 		/*Panel zur HTML Seite hinzufügen */ 
-		 RootPanel.get().add(mainPanel);
+		 RootPanel.get("projektmarktplatz").add(mainPanel);
 		
 		
 	}
