a7eec06851f232488f10aa4571f2913050f1788c
Daniel Fleps <dfleps@live.de>
2017-06-07 16:17:36 +0200

    Erweiterung ClientsideSettings

--- src/de/hdm/it_projekt/client/ClientsideSettings.java
+++ src/de/hdm/it_projekt/client/ClientsideSettings.java
@@ -10,7 +10,7 @@ import de.hdm.it_projekt.shared.*;
 
 public class ClientsideSettings extends CommonSettings {
 
-	 private static ProjektAdministrationAsync projektVerwaltung = null;
+	 private static ProjektAdministrationAsync projektAdministration = null;
 	 
 	 private static ReportGeneratorAsync reportGenerator = null;
 	 
@@ -29,5 +29,45 @@ public class ClientsideSettings extends CommonSettings {
 		    return log;
 		  }
 	  
+	  
+	  public static ProjektAdministrationAsync getProjektAdministration() {
+		    // Erzeugen einer ProjektAdministrations Instanz wenn noch keine vorhanden ist. 
+		    if (projektAdministration == null) {
+		      // projektAdministration wird instanziert 
+		    	projektAdministration = GWT.create(ProjektAdministration.class);
+		    }
+
+		    // Rückgabe der projektAdministration. 
+		    return projektAdministration;
+		  }
+	  
+	  
+	  
+	  public static ReportGeneratorAsync getReportGenerator() {
+		    //Erzeugen einer ReportGenerator Instanz wenn noch keine vorhanden ist. 
+		    if (reportGenerator == null) {
+		      // Zunächst instantiieren wir ReportGenerator
+		      reportGenerator = GWT.create(ReportGenerator.class);
+
+		      final AsyncCallback<Void> initReportGeneratorCallback = new AsyncCallback<Void>() {
+		        @Override
+				public void onFailure(Throwable caught) {
+		          ClientsideSettings.getLogger().severe(
+		              "Der Report Generator konnte nicht initialisiert werden.");
+		        }
+
+		        @Override
+				public void onSuccess(Void result) {
+		          ClientsideSettings.getLogger().info(
+		              "Der Report Generator wurde initialisiert.");
+		        }
+		      };
+
+		      reportGenerator.init(initReportGeneratorCallback);
+		    }
+
+		    // Rückgabe des Report Generators 
+		    return reportGenerator;
+		  }
 	  
 }
