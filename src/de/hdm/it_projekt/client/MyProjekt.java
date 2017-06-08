15ee7af4a03940f082098a9076fd48308331a2cb
Daniel Miedtank <d.miedtank@miedtank.com>
2017-06-08 19:28:37 +0200

    Database Testing

--- src/de/hdm/it_projekt/client/MyProjekt.java
+++ src/de/hdm/it_projekt/client/MyProjekt.java
@@ -28,13 +28,13 @@ public class MyProjekt implements EntryPoint {
 		ProjektAdministrationAsync pa = ClientsideSettings.getProjektAdministration();
 
 
-
 		final Label ausgabe = new Label();
-		ausgabe.setText("Hallo<br>");
+		ausgabe.setText("Hallo_!");
 
 		final HorizontalPanel mainPanel = new HorizontalPanel();
 		
 		pa.getAlleProjektMarktplaetze(new Marktplaetze(ausgabe));
+		
 		
 		final MarktplatzUebersicht menu = new MarktplatzUebersicht();
 		
@@ -59,7 +59,7 @@ public class MyProjekt implements EntryPoint {
 		@Override
 		public void onFailure(Throwable caught) {
 
-			a.setText("Fehler bei getAlleMarktplätze." + caught.getMessage());
+			a.setText(caught.getMessage());
 		}
 
 		@Override
