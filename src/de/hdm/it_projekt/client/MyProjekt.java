39adf90d938e0765541879da8d3c37a194eeae5d
SidHeiland <sh244@hdm-stuttgart.de>
2017-06-09 15:04:39 +0200

    Login am EntryPoint eigefuegt. Webxml fuer Login konfiguriert

--- src/de/hdm/it_projekt/client/MyProjekt.java
+++ src/de/hdm/it_projekt/client/MyProjekt.java
@@ -1,6 +1,6 @@
 package de.hdm.it_projekt.client;
 
-import java.util.Vector;
+import java.util.Vector; 
 
 import com.google.gwt.core.client.*;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -14,6 +14,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Anchor;
 
 import de.hdm.it_projekt.client.GUI.MarktplatzUebersicht;
 import de.hdm.it_projekt.shared.ProjektAdministrationAsync;
@@ -21,19 +22,62 @@ import de.hdm.it_projekt.shared.bo.ProjektMarktplatz;
 
 public class MyProjekt implements EntryPoint {
 
-	public void onModuleLoad() {
+	private LoginInfo loginInfo = null;
+	private VerticalPanel loginPanel = new VerticalPanel();
+	private Label loginLabel = new Label (
+			"Bitte eilogge... Ahnma!.");
+	private Anchor signInLink = new Anchor("Sign In");
 
-		ProjektAdministrationAsync pa = ClientsideSettings.getProjektAdministration();
+	public void onModuleLoad() {
+		//Check login status using login service
+		LoginServiceAsync loginService = GWT.create(LoginService.class);
+		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
+			public void onFailure(Throwable error) {
+			}
+		
+		public void onSuccess(LoginInfo result) {
+			loginInfo = result;
+			if(loginInfo.isLoggedIn()) {
+				loadMyProjekt();
+			} else {
+				loadLogin();
+			}}});}
+				
+			
+
+	private void loadLogin() {
+		//Assemble login panel
+		signInLink.setHref(loginInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get("stockList").add(loginPanel);
+	}
+		
+		
+
+		
+		private void loadMyProjekt() {
+			final HorizontalPanel mainPanel = new HorizontalPanel();
+			final Label ausgabe = new Label("test");
+			mainPanel.add(ausgabe);
+			RootPanel.get("content").add(mainPanel);	
+		}
+	}
+
+		
+
+
+	/*	ProjektAdministrationAsync pa = ClientsideSettings.getProjektAdministration();
 
 		final HorizontalPanel mainPanel = new HorizontalPanel();
 		final Label ausgabe = new Label();
 		pa.getAlleProjektMarktplaetze(new Marktplaetze(ausgabe));
 
 		/* main Panel */ 
-	    mainPanel.add(ausgabe); 
+	 /*   mainPanel.add(ausgabe); */
 		
 		/*Panel zur HTML Seite hinzufügen */ 
-		 RootPanel.get("content").add(mainPanel);
+		/* RootPanel.get("content").add(mainPanel);
 		 
 	}
 
@@ -60,6 +104,6 @@ public class MyProjekt implements EntryPoint {
 			}
 			
 			a.setText(t);			
-		}
-	}
-}
+		} */
+	
+
