743ca3383628ee399f9b306d9a8ae9d0767d108d
SidHeiland <sh244@hdm-stuttgart.de>
2017-06-13 13:12:38 +0200

    Testmain wiederhergetsellt

---
+++ src/de/hdm/it_projekt/shared/bo/Testmain.java
@@ -0,0 +1,57 @@
+package de.hdm.it_projekt.shared.bo;
+
+import de.hdm.it_projekt.server.db.*;  
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+public class Testmain {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		
+		
+		/*Mapper Definitionen*/
+		AusschreibungMapper asMapper = AusschreibungMapper.ausschreibungMapper();
+		BeteiligungMapper btMapper = BeteiligungMapper.beteiligungMapper();
+		BewerbungMapper bwMapper = BewerbungMapper.bewerbungMapper();
+		BewertungMapper bwtMapper = BewertungMapper.bewertungMapper();
+		EigenschaftMapper egMapper = EigenschaftMapper.eigenschaftMapper();
+		PartnerprofilMapper pfMapper = PartnerprofilMapper.partnerprofilMapper();
+		PersonMapper psMapper = PersonMapper.personMapper();
+		ProjektMapper pjMapper = ProjektMapper.projektMapper();
+		ProjektMarktplatzMapper pmMapper = ProjektMarktplatzMapper.projektMarktplatzMapper();
+		TeamMapper tmMapper = TeamMapper.teamMapper();
+		UnternehmenMapper unMapper = UnternehmenMapper.unternehmenMapper();
+		/*Ende Mapper Definitionen*/
+
+		//System.out.println(pm1.getId() + " " + pm1.getBezeichnung());
+
+		/*
+		p1.setBezeichnung("Volvo");
+		pmMapper.update(p1);
+		pmMapper.delete(p1);
+		*/
+		
+		System.out.println(tmMapper.findTeamByGoogleId("oie8tu49hgnovrhe"));
+		System.out.println(unMapper.findUnternehmenByGoogleId("klsnjv98z793rhgobwv"));
+		System.out.println(psMapper.findPersonByGoogleId("iurehf934fh3ovrv"));
+		
+		System.out.println(tmMapper.findByMail("info@goodquality.de"));
+		System.out.println(tmMapper.findAll());
+		
+		
+		ProjektMarktplatz pm1 = new ProjektMarktplatz();
+		pm1 = pmMapper.findById(4);
+		System.out.println(psMapper.getByProjektMarktplatz(pm1));
+		
+		
+		/*System.out.println(readDate());*/
+
+	}
+
+}
