a5a330944eed2bb4e3add142372824952864630f
SidHeiland <sh244@hdm-stuttgart.de>
2017-06-27 11:41:17 +0200

    Report Generator erweitert

--- src/de/hdm/it_projekt/client/GUI_Report/ReportGenerator.java
+++ src/de/hdm/it_projekt/client/GUI_Report/ReportGenerator.java
@@ -10,6 +10,7 @@ import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 public class ReportGenerator implements EntryPoint {
@@ -18,25 +19,70 @@ public class ReportGenerator implements EntryPoint {
 	
 	public void onModuleLoad() {
 
-		final HorizontalPanel testpanel = new HorizontalPanel();
-		Grid form = new Grid (2,2);
-		
-		Label text = new Label("Hallo");
-		Button einButton = new Button("BTText");
-		einButton.setStyleName("myprojekt-formbutton");
-		form.setWidget(0, 0, einButton);
-		form.setWidget(0, 1, text);
-		einButton.addClickHandler(new ChangeClickhandler());
-		testpanel.add(form);
+		/**
+		 * Definition der Panels
+		 */
+		final VerticalPanel vertPanelNav = new VerticalPanel();
 
 		
-		RootPanel.get("content").add(new Label("Report Generator"));
-		RootPanel.get("content").add(testpanel);
+		/**
+		 * Button Definitionen
+		 */
+		Button alleAusschreibungenButton = new Button("Zeige Alle Ausschreibungen");
+		alleAusschreibungenButton.setStyleName("myprojekt-reportbutton");
+		alleAusschreibungenButton.addClickHandler(new ChangeClickhandler());
+		
+		Button passendeAusschreibungenButton = new Button("Zeige passende Ausschreibungen");
+		passendeAusschreibungenButton.setStyleName("myprojekt-reportbutton");
+		passendeAusschreibungenButton.addClickHandler(new ChangeClickhandler());
+		
+		Button bewerbungAufAusschreibungenButton = new Button("Zeige Bewerbungen für Ausschreibung");
+		bewerbungAufAusschreibungenButton.setStyleName("myprojekt-reportbutton");
+		bewerbungAufAusschreibungenButton.addClickHandler(new ChangeClickhandler());
+		
+		Button ausschreibungenAufBewerbungButton = new Button("Zeige Ausschreibung zur Bewerbung");
+		ausschreibungenAufBewerbungButton.setStyleName("myprojekt-reportbutton");
+		ausschreibungenAufBewerbungButton.addClickHandler(new ChangeClickhandler());
+		
+		Button fanOutButton = new Button("Zeige Fan Out");
+		fanOutButton.setStyleName("myprojekt-reportbutton");
+		fanOutButton.addClickHandler(new ChangeClickhandler());
+		
+		Button fanInButton = new Button("Zeige Fan In");
+		fanInButton.setStyleName("myprojekt-reportbutton");
+		fanInButton.addClickHandler(new ChangeClickhandler());
+		
+		Button zugehoerigeProjekteButton = new Button("Zeige zugehoerige Projekte");
+		zugehoerigeProjekteButton.setStyleName("myprojekt-reportbutton");
+		zugehoerigeProjekteButton.addClickHandler(new ChangeClickhandler());
+		
+		/**
+		 * Hinzufuegen der Buttons zum vertikel Panel
+		 */
+		vertPanelNav.add(alleAusschreibungenButton);
+		vertPanelNav.add(passendeAusschreibungenButton);
+		vertPanelNav.add(bewerbungAufAusschreibungenButton);
+		vertPanelNav.add(ausschreibungenAufBewerbungButton);
+		vertPanelNav.add(fanOutButton);
+		vertPanelNav.add(fanInButton);
+		vertPanelNav.add(zugehoerigeProjekteButton);
+
+		/**
+		 * Ueberschirft fuer den Rpeort Generator
+		 */
+		Label ueberschriftLabel = new Label("Report Generator");
+		ueberschriftLabel.setStyleName("h1");
+		
+		/**
+		 * Zusammenfuegen des Panels
+		 */
+		RootPanel.get("content").add(ueberschriftLabel);
+		RootPanel.get("content").add(vertPanelNav);
 	}
 		
 	private class ChangeClickhandler implements ClickHandler {
 		public void onClick(ClickEvent event) {
-			Window.alert("test");
+			Window.alert("tbd");
 		}
 	}	
 
